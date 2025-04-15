c34178caccc5de3b59caf38db16d1c637026d91e
Xinyao Qiu <xinyaoq3@illinois.edu>
2025-04-15 16:53:11 -0500

    Remove Element

---
+++ leetcode/top150/27.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int i = 0, j = n - 1;
+        while (i <= j) {
+            if (nums[i] == val) {
+                swap(nums[i], nums[j--]);
+            } else {
+                i++;
+            }
+        }
+        return i;
+    }
+};
