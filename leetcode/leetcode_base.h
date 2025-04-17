ab5af67b70de0467bd418df1017a5967951931bb
Xinyao Qiu <xylonqiu@gmail.com>
2025-04-17 14:16:27 -0500

    update leetcode_base.h

--- leetcode/leetcode_base.h
+++ leetcode/leetcode_base.h
@@ -1,14 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <cmath>
 #include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <numeric>
 #include <queue>
 #include <string>
-#include <cctype>
-#include <numeric>
-#include <climits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
