51ac4a18209c8df771d88fec30f33a7807483612
Xinyao Qiu <xylonqiu@gmail.com>
2025-04-18 18:09:08 -0500

    Remove Duplicates from Sorted Array II

--- leetcode/top150/80.cpp
+++ leetcode/top150/80.cpp
@@ -4,12 +4,13 @@ using namespace std;
 class Solution {
   public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size(), i = 2;
-        for (int j = 2; j < n; j++) {
-            if (nums[j] != nums[i - 2]) {
-                nums[i++] = nums[j];
+        int i = 0, n = nums.size();
+        for (int j = 0; j < n; j++) {
+            if (i > 1 && nums[j] == nums[i - 2]) {
+                continue;
             }
+            nums[i++] = nums[j];
         }
-        return min(n, i);
+        return i;
     }
 };
