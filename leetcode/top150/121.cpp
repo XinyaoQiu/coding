14c15c0b45994533b06e6511f64dd16cac930b2a
Xinyao Qiu <xylonqiu@gmail.com>
2025-04-24 20:23:27 -0500

    Best Time to Buy and Sell Stock

---
+++ leetcode/top150/121.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices) {
+        int start = INT_MAX, ans = 0;
+        for (int price : prices) {
+            start = min(start, price);
+            ans = max(ans, price - start);
+        }
+        return ans;
+    }
+};
