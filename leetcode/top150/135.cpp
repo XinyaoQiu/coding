151cd59072d6291dabafb0d16ec7a8928ad2a0fe
Xinyao Qiu <xylonqiu@gmail.com>
2025-04-29 21:35:19 -0500

    Candy

---
+++ leetcode/top150/135.cpp
@@ -0,0 +1,34 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+/*
+    1, 2, 1, 1, 1
+    1, 2, 1, 2, 1
+*/
+
+class Solution {
+  public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> dp(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                dp[i] = max(dp[i], dp[i - 1] + 1);
+            }
+        }
+        int count = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                dp[i] = max(dp[i], ++count);
+            } else {
+                count = 1;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += dp[i];
+        }
+        return ans;
+    }
+};
