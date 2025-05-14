306ed3a38721021ff1211b592f62292fa147b693
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-13 21:30:30 -0500

    Container With Most Water

---
+++ leetcode/top150/11.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int maxArea(vector<int>& height) {
+        int i = 0, j = height.size() - 1;
+        int res = 0;
+        while (i < j) {
+            if (height[i] < height[j]) {
+                res = max(res, height[i] * (j - i));
+                i++;
+            } else {
+                res = max(res, height[j] * (j - i));
+                j--;
+            }
+        }
+        return res;
+    }
+};
