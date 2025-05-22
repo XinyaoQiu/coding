d942bbb5f24832dac22227960725126b26a9def4
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-22 21:57:17 +0800

    Minimum Size Subarray Sum

--- leetcode/top150/209.cpp
+++ leetcode/top150/209.cpp
@@ -4,14 +4,15 @@ using namespace std;
 class Solution {
   public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int res = nums.size() + 1, curr = 0;
-        for (int i = 0, j = 0; j < nums.size(); j++) {
+        int n = nums.size(), res = n + 1, curr = 0;
+        for (int i = 0, j = 0; j < n; j++) {
             curr += nums[j];
             while (curr >= target) {
                 res = min(res, j - i + 1);
-                curr -= nums[i++];
+                curr -= nums[i];
+                i++;
             }
         }
-        return res > nums.size() ? 0 : res;
+        return res > n ? 0 : res;
     }
 };
