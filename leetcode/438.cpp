ce3a00421405f0cfc33c95de37cd6a7257acd91b
qiuxinyao <qiuxinyao@bytedance.com>
2025-05-23 10:58:50 +0800

    Find All Anagrams in a String
    Change-Id: I085d4df4ca66e1035162924c47692bc22c2f0313

--- leetcode/438.cpp
+++ leetcode/438.cpp
@@ -6,27 +6,41 @@ class Solution {
   public:
     vector<int> findAnagrams(string s, string p) {
         int sl = s.size(), pl = p.size();
-        if (sl < pl)
+        if (sl < pl) {
             return {};
-        vector<int> res;
-        vector<int> s_count(26);
-        vector<int> p_count(26);
+        }
+        vector<int> ans;
+        vector<int> count(26);
         for (int i = 0; i < pl; ++i) {
-            ++s_count[s[i] - 'a'];
-            ++p_count[p[i] - 'a'];
+            ++count[s[i] - 'a'];
+            --count[p[i] - 'a'];
         }
-        if (s_count == p_count) {
-            res.push_back(0);
-        }
-        for (int i = 0; i + pl < sl; ++i) {
-            int in = s[i + pl] - 'a', out = s[i] - 'a';
-            ++s_count[in];
-            --s_count[out];
-            if (s_count[in] == p_count[in] && s_count[out] == p_count[out] &&
-                s_count == p_count) {
-                res.push_back(i + 1);
+        int diff = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] != 0) {
+                ++diff;
             }
         }
-        return res;
+        if (diff == 0) {
+            ans.push_back(0);
+        }
+        for (int i = 0; i + pl < sl; ++i) {
+            if (count[s[i] - 'a'] == 0) {
+                ++diff;
+            } else if (count[s[i] - 'a'] == 1) {
+                --diff;
+            }
+            --count[s[i] - 'a'];
+            if (count[s[i + pl] - 'a'] == 0) {
+                ++diff;
+            } else if (count[s[i + pl] - 'a'] == -1) {
+                --diff;
+            }
+            ++count[s[i + pl] - 'a'];
+            if (diff == 0) {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
     }
 };
