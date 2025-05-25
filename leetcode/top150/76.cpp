71c5df0868bbc99a2c0e8798674c2bdf997f4b83
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-25 18:45:57 +0800

    Minimum Window Substring

--- leetcode/top150/76.cpp
+++ leetcode/top150/76.cpp
@@ -3,36 +3,28 @@
 using namespace std;
 
 class Solution {
-    bool verify(vector<int>& s_count, vector<int>& t_count) {
-        for (int i = 'a'; i <= 'z'; ++i) {
-            if (s_count[i] < t_count[i]) {
-                return false;
-            }
-        }
-        for (int i = 'A'; i <= 'Z'; ++i) {
-            if (s_count[i] < t_count[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
   public:
     string minWindow(string s, string t) {
         int sl = s.size(), tl = t.size();
-        int ans_start = -1, ans_len = sl + 1;
-        vector<int> s_count(128), t_count(128);
+        int ans_start = -1, ans_len = sl + 1, diff = 0;
+        vector<int> count(128); // t-count > s-count
         for (char c : t) {
-            ++t_count[c];
+            if (++count[c] == 1) {
+                ++diff;
+            }
         }
         for (int i = 0, j = 0; j < sl; ++j) {
-            ++s_count[s[j]];
-            while (i <= j && verify(s_count, t_count)) {
+            if (--count[s[j]] == 0) {
+                --diff;
+            }
+            while (i <= j && diff == 0) {
                 if (j - i + 1 < ans_len) {
                     ans_start = i;
                     ans_len = j - i + 1;
                 }
-                --s_count[s[i++]];
+                if (++count[s[i++]] == 1) {
+                    ++diff;
+                }
             }
         }
         return ans_start >= 0 ? s.substr(ans_start, ans_len) : "";
