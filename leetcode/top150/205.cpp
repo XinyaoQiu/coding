aff95070c98357019a01ea44826d7ab7030e31c8
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-28 18:35:06 +0800

    Isomorphic Strings
    Change-Id: I0add5edea9819008a7b459121d977f6c778bd1e2

--- leetcode/top150/205.cpp
+++ leetcode/top150/205.cpp
@@ -6,14 +6,17 @@ using namespace std;
 class Solution {
   public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> m_s;
-        unordered_map<char, char> m_t;
+        unordered_map<char, char> s_to_t;
+        unordered_map<char, char> t_to_s;
         for (int i = 0; i < s.size(); ++i) {
-            if ((m_s.count(s[i]) && m_s[s[i]] != t[i]) || (m_t.count(t[i]) && m_t[t[i]] != s[i])) {
+            if (s_to_t.count(s[i]) && s_to_t[s[i]] != t[i]) {
                 return false;
             }
-            m_s[s[i]] = t[i];
-            m_t[t[i]] = s[i];
+            if (t_to_s.count(t[i]) && t_to_s[t[i]] != s[i]) {
+                return false;
+            }
+            s_to_t[s[i]] = t[i];
+            t_to_s[t[i]] = s[i];
         }
         return true;
     }
