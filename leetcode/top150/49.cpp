eca3fb97d928120f7e5e61927d6fce5dec6aecc3
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-29 19:23:17 +0800

    Group Anagrams
    Change-Id: I41d354459023165a12545f0f403350c445336eec

--- leetcode/top150/49.cpp
+++ leetcode/top150/49.cpp
@@ -1,19 +1,31 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
+#include <array>
+#include <numeric>
 using namespace std;
 
+// c++20
+
 class Solution {
+    struct ArrayHash {
+        size_t operator()(const array<int, 26>& arr) const {
+            return accumulate(arr.begin(), arr.end(), 0u, [&](size_t acc, int num) {
+                return (acc << 1) ^ hash<int>{}(num);
+            });
+        }
+    };
   public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> m;
-        for (string& str : strs) {
-            string sorted_str = str;
-            sort(sorted_str.begin(), sorted_str.end());
-            m[sorted_str].emplace_back(str);
-        }
         vector<vector<string>> ans;
+        unordered_map<array<int, 26>, vector<string>, ArrayHash> m;
+        for (string& s : strs) {
+            array<int, 26> arr{};
+            for (char c : s) {
+                ++arr[c - 'a'];
+            }
+            m[arr].emplace_back(s);
+        }
         for (auto& [k, v] : m) {
             ans.emplace_back(v);
         }
