59449b6af7c26f35f164c9f7461c41b1221f0c84
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-29 10:20:35 +0800

    2
    Change-Id: I8bdb623ad02ffb48b1febefd40cb3ebf53a5247f

--- leetcode/top150/242.cpp
+++ leetcode/top150/242.cpp
@@ -5,6 +5,16 @@ using namespace std;
 class Solution {
   public:
     bool isAnagram(string s, string t) {
-        
+        vector<int> count(26);
+        for (char c : s) {
+            ++count[c - 'a'];
+        }
+        for (char c : s) {
+            --count[c - 'a'];
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
