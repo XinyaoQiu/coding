27ef36c99ba18496ef40841e954f1e019a1522b7
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-29 19:52:53 +0800

    Contains Duplicate II
    Change-Id: I50f473a23ca96b867d993f9fde3b0184f3c678af

---
+++ leetcode/top150/219.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+  public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (i > k) {
+                s.erase(nums[i - k - 1]);
+            }
+            if (s.count(nums[i])) {
+                return true;
+            }
+            s.emplace(nums[i]);
+        }
+        return false;
+    }
+};
