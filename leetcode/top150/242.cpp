d2cc48e483fdf48acbdb5ae80459cff9f588bf85
Xinyao Qiu <xylonqiu@gmail.com>
2025-05-29 10:18:32 +0800

    1
    Change-Id: I4acb21e5c26e0dc29bf254d8ddecdae483236c6a

--- leetcode/top150/242.cpp
+++ leetcode/top150/242.cpp
@@ -5,21 +5,6 @@ using namespace std;
 class Solution {
   public:
     bool isAnagram(string s, string t) {
-        vector<int> count(26);
-        for (char c : s) {
-            count[c - 'a']++;
-        }
-        for (char c : t) {
-            count[c - 'a']--;
-            if (count[c - 'a'] < 0) {
-                return false;
-            }
-        }
-        for (int x : count) {
-            if (x > 0) {
-                return false;
-            }
-        }
-        return true;
+        
     }
 };
--- leetcode/top150/290.cpp
+++ leetcode/top150/290.cpp
@@ -1,36 +1,32 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 class Solution {
   public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char, string> p_to_s;
-        unordered_map<string, char> s_to_p;
+        istringstream iss(s);
         vector<string> words;
         string word;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] != ' ') {
-                word += s[i];
-            } else {
-                words.emplace_back(word);
-                word = "";
-            }
+        while (iss >> word) {
+            words.emplace_back(word);
         }
-        words.emplace_back(word);
         if (pattern.size() != words.size()) {
             return false;
         }
+        unordered_map<char, string> ctos;
+        unordered_map<string, char> stoc;
         for (int i = 0; i < pattern.size(); ++i) {
-            if (p_to_s.count(pattern[i]) && p_to_s[pattern[i]] != words[i]) {
+            if (ctos.count(pattern[i]) && ctos[pattern[i]] != words[i]) {
                 return false;
             }
-            if (s_to_p.count(words[i]) && s_to_p[words[i]] != pattern[i]) {
+            if (stoc.count(words[i]) && stoc[words[i]] != pattern[i]) {
                 return false;
             }
-            p_to_s[pattern[i]] = words[i];
-            s_to_p[words[i]] = pattern[i];
+            ctos[pattern[i]] = words[i];
+            stoc[words[i]] = pattern[i];
         }
         return true;
     }
