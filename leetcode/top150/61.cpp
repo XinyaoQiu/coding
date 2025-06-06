a4586b5249780539ebcd31776b319b43d2c9f654
Xinyao Qiu <xylonqiu@gmail.com>
2025-06-06 20:08:15 +0800

    Rotate List

--- leetcode/top150/61.cpp
+++ leetcode/top150/61.cpp
@@ -11,47 +11,33 @@
 
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
-    void reverse(ListNode* p0, int k) {
-        if (!p0->next || !p0->next->next || k == 0) {
-            return;
-        }
-        ListNode* cur = p0->next, *pre = nullptr, *nxt = nullptr;
-        for (int i = 0; i < k; ++i) {
-            nxt = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = nxt;
-        }
-        p0->next->next = cur;
-        p0->next = pre;
-    }
   public:
     ListNode* rotateRight(ListNode* head, int k) {
         if (!head) {
             return nullptr;
         }
-        ListNode* dummy = new ListNode(0, head);
         int len = 0;
         ListNode* cur = head;
-        while (cur) {
+        while (cur->next) {
             ++len;
             cur = cur->next;
         }
-        k = k % len;
-        reverse(dummy, len);
-        reverse(dummy, k);
-        cur = dummy;
-        for (int i = 0; i < k; ++i) {
+        ++len;
+        cur->next = head;
+        k = len - (k % len);
+        cur = head;
+        for (int i = 0; i < k - 1; ++i) {
             cur = cur->next;
         }
-        reverse(cur, len - k);
-        return dummy->next;
+        head = cur->next;
+        cur->next = nullptr;
+        return head;
     }
 };
