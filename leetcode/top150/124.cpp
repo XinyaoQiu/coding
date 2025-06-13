60f3643f269dcc1745b3924af86d78c22bd5dc79
Xinyao Qiu <xylonqiu@gmail.com>
2025-06-13 14:58:24 +0800

    Binary Tree Maximum Path Sum
    Change-Id: I0cf867748a44903e9eb2ba180a041fe995e19166

---
+++ leetcode/top150/124.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    int ans = INT_MIN;
+    int dfs(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int left = max(dfs(root->left), 0);
+        int right = max(dfs(root->right), 0);
+        ans = max(ans, left + right + root->val);
+        return root->val + max(left, right);
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
