0eea1e0ee6f3762fd8ba123f69d967b6e843ab7f
Xinyao Qiu <xylonqiu@gmail.com>
2025-06-14 22:23:39 +0800

    1

--- leetcode/top150/105.cpp
+++ leetcode/top150/105.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 struct TreeNode {
     int val;
@@ -11,25 +12,23 @@ struct TreeNode {
 };
 
 class Solution {
+    unordered_map<int, int> m;
     TreeNode* build(vector<int>& preorder, vector<int>& inorder, int pL, int pR, int iL, int iR) {
         if (pL > pR) {
             return nullptr;
         }
         int root_val = preorder[pL];
-        int index = 0;
-        for (int i = iL; i <= iR; ++i) {
-            if (inorder[i] == root_val) {
-                index = i;
-                break;
-            }
-        }
-        TreeNode* left = build(preorder, inorder, pL + 1, pL + index - iL, iL, index - 1);
-        TreeNode* right = build(preorder, inorder, pL + index - iL + 1, pR, index + 1, iR);
+        int iM = m[root_val];
+        TreeNode* left = build(preorder, inorder, pL + 1, pL + iM - iL, iL, iM - 1);
+        TreeNode* right = build(preorder, inorder, pL + iM - iL + 1, pR, iM + 1, iR);
         return new TreeNode(root_val, left, right);
     }
   public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int pL = 0, pR = preorder.size() - 1, iL = 0, iR = inorder.size() - 1;
+        for (int i = iL; i <= iR; ++i) {
+            m[inorder[i]] = i;
+        }
         return build(preorder, inorder, pL, pR, iL, iR);
     }
 };
--- leetcode/top150/106.cpp
+++ leetcode/top150/106.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 struct TreeNode {
     int val;
@@ -11,24 +12,22 @@ struct TreeNode {
 };
 
 class Solution {
+    unordered_map<int, int> m;
     TreeNode* build(vector<int>& postorder, vector<int>& inorder, int pL, int pR, int iL, int iR) {
         if (pL > pR) {
             return nullptr;
         }
         int val = postorder[pR];
-        int iM = 0;
-        for (int i = iL; i <= iR; ++i) {
-            if (inorder[i] == val) {
-                iM = i;
-                break;
-            }
-        }
+        int iM = m[val];
         TreeNode* left = build(postorder, inorder, pL, pL + iM - iL - 1, iL, iM - 1);
         TreeNode* right = build(postorder, inorder, pL + iM - iL, pR - 1, iM + 1, iR);
         return new TreeNode(val, left, right);
     }
   public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        for (int i = 0; i < inorder.size(); ++i) {
+            m[inorder[i]] = i;
+        }
         return build(postorder, inorder, 0, postorder.size() - 1, 0, inorder.size() - 1);
     }
 };
