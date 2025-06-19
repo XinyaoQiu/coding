7606ca765f81ee2c55b59d4396985224476bcf33
Xinyao Qiu <xylonqiu@gmail.com>
2025-06-19 20:30:52 +0800

    Number of Islands
    Change-Id: Ibea90a664e3e5e7c37d3b545a72b77717d83ea3f

---
+++ leetcode/top150/200.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size(), n = grid[0].size();
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+        if (grid[i][j] == '1') {
+            grid[i][j] = '2';
+            dfs(grid, i - 1, j);
+            dfs(grid, i + 1, j);
+            dfs(grid, i, j - 1);
+            dfs(grid, i, j + 1);
+        }
+    }
+public:
+    const int DIR[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int count = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    ++count;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
