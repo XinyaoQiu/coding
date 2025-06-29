a8a163e6f74e51f8300420ff08ab35d8e71508d9
Xinyao Qiu <xylonqiu@gmail.com>
2025-06-29 18:46:32 +0800

    another way to solve Word Ladder
    Change-Id: Ie76d500a92b93de9b2b43cb086fa4049ad85aace

--- leetcode/top150/127.cpp
+++ leetcode/top150/127.cpp
@@ -4,33 +4,54 @@
 #include <unordered_map>
 #include <utility>
 #include <queue>
+#include <climits>
 using namespace std;
 
 class Solution {
+    unordered_map<string, int> word2id;
+    vector<vector<int>> edge;
+    int word_num = 0;
+    void add_word(string& word) {
+        if (!word2id.count(word)) {
+            word2id[word] = word_num++;
+            edge.emplace_back();
+        }
+    }
+    void add_edge(string& word) {
+        add_word(word);
+        int id1 = word2id[word];
+        for (char& it : word) {
+            char tmp = it;
+            it = '*';
+            add_word(word);
+            int id2 = word2id[word];
+            edge[id1].push_back(id2);
+            edge[id2].push_back(id1);
+            it = tmp;
+        }
+    }
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        unordered_map<string, int> visited;
-        queue<pair<string, int>> q;
-        int len = beginWord.size();
-        q.emplace(beginWord, 1);
+        for (string& word : wordList) {
+            add_edge(word);
+        }
+        if (!word2id.count(endWord)) {
+            return 0;
+        }
+        add_edge(beginWord);
+        queue<int> q({word2id[beginWord]});
+        vector<int> visited(word2id.size(), -1);
+        visited[word2id[beginWord]] = 0;
         while (!q.empty()) {
-            auto [word, step] = q.front();
+            int u = q.front();
             q.pop();
-            for (int i = 0; i < len; ++i) {
-                for (int j = 0; j < 26; ++j) {
-                    string newWord = word;
-                    newWord.replace(i, 1, string(1, 'a' + j));
-                    if (!wordSet.count(newWord)) {
-                        continue;
-                    }
-                    if (newWord == endWord) {
-                        return step + 1;
-                    }
-                    if (!visited[newWord]) {
-                        visited[newWord] = 1;
-                        q.emplace(newWord, step + 1);
-                    }
+            for (int v : edge[u]) {
+                if (visited[v] == -1) {
+                    visited[v] = visited[u] + 1;
+                    q.push(v);
+                }
+                if (v == word2id[endWord]) {
+                    return visited[v] / 2 + 1;
                 }
             }
         }
