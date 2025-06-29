d37bb99b0131a2ce51c45721fb4db1bc23a36de4
Xinyao Qiu <xylonqiu@gmail.com>
2025-06-29 19:23:56 +0800

    1
    Change-Id: I00a972630b1771a2f8af11f792992051ebeabd18

--- leetcode/top150/127.cpp
+++ leetcode/top150/127.cpp
@@ -8,51 +8,35 @@
 using namespace std;
 
 class Solution {
-    unordered_map<string, int> word2id;
-    vector<vector<int>> edge;
-    int word_num = 0;
-    void add_word(string& word) {
-        if (!word2id.count(word)) {
-            word2id[word] = word_num++;
-            edge.emplace_back();
-        }
-    }
-    void add_edge(string& word) {
-        add_word(word);
-        int id1 = word2id[word];
-        for (char& it : word) {
-            char tmp = it;
-            it = '*';
-            add_word(word);
-            int id2 = word2id[word];
-            edge[id1].push_back(id2);
-            edge[id2].push_back(id1);
-            it = tmp;
-        }
-    }
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        for (string& word : wordList) {
-            add_edge(word);
-        }
-        if (!word2id.count(endWord)) {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (!wordSet.count(endWord)) {
             return 0;
         }
-        add_edge(beginWord);
-        queue<int> q({word2id[beginWord]});
-        vector<int> visited(word2id.size(), -1);
-        visited[word2id[beginWord]] = 0;
+        queue<string> q({beginWord});
+        unordered_map<string, int> step;
+        step[beginWord] = 1;
         while (!q.empty()) {
-            int u = q.front();
+            string word = q.front();
             q.pop();
-            for (int v : edge[u]) {
-                if (visited[v] == -1) {
-                    visited[v] = visited[u] + 1;
-                    q.push(v);
+            int currStep = step[word];
+            for (char& it : word) {
+                char tmp = it;
+                for (int i = 'a'; i <= 'z'; ++i) {
+                    it = i;
+                    if (!wordSet.count(word)) {
+                        continue;
+                    }
+                    if (word == endWord) {
+                        return currStep + 1;
+                    }
+                    if (step[word] == 0) {
+                        step[word] = currStep + 1;
+                        q.emplace(word);
+                    }
                 }
-                if (v == word2id[endWord]) {
-                    return visited[v] / 2 + 1;
-                }
+                it = tmp;
             }
         }
         return 0;
