a994814bec6f23fa1386a77d92b90ad8ee80f823
Xinyao Qiu <xylonqiu@gmail.com>
2025-07-08 18:20:15 +0800

    Word Search
    Change-Id: I1d9d8333be867a5cfe690fe887930ef721e295a4

--- leetcode/top150/79.cpp
+++ leetcode/top150/79.cpp
@@ -3,32 +3,23 @@
 using namespace std;
 
 class Solution {
-    string path;
     vector<vector<int>> visited;
     const int DIR[4][4] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j) {
-        if (path == word) {
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int index) {
+        if (index == word.size()) {
             return true;
         }
-        if (path != "" && path.back() != word[path.size() - 1]) {
+        int m = board.size(), n = board[0].size();
+        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || board[i][j] != word[index]) {
             return false;
         }
-        int m = board.size(), n = board[0].size();
+        visited[i][j] = 1;
         for (int k = 0; k < 4; ++k) {
-            i += DIR[k][0];
-            j += DIR[k][1];
-            if (i > 0 && i < m - 1 && j > 0 && j < n - 1 && !visited[i][j]) {
-                path.push_back(board[i][j]);
-                visited[i][j] = 1;
-                if (dfs(board, word, i, j)) {
-                    return true;
-                }
-                visited[i][j] = 0;
-                path.pop_back();
+            if (dfs(board, word, i + DIR[k][0], j + DIR[k][1], index + 1)) {
+                return true;
             }
-            i -= DIR[k][0];
-            j -= DIR[k][1];
         }
+        visited[i][j] = 0;
         return false;
     }
 public:
@@ -37,15 +28,11 @@ public:
         visited = vector<vector<int>>(m, vector<int>(n));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                path.push_back(board[i][j]);
-                visited[i][j] = 1;
-                if (dfs(board, word, i, j)) {
+                if (dfs(board, word, i, j, 0)) {
                     return true;
                 }
-                visited[i][j] = 0;
-                path.pop_back();
             }
         }
-        return false;;
+        return false;
     }
 };
