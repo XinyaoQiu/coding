aec870bf094088c9e18709cbcf6d1799a938bd45
Xinyao Qiu <xylonqiu@gmail.com>
2025-07-19 18:47:04 +0800

    502. IPO

--- leetcode/top150/502.cpp
+++ leetcode/top150/502.cpp
@@ -1,24 +1,26 @@
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        priority_queue<int, vector<int>, less<int>> pq;
         vector<pair<int, int>> capitals;
         int n = profits.size();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; ++i) {
             capitals.emplace_back(capital[i], profits[i]);
         }
         sort(capitals.begin(), capitals.end());
+        priority_queue<int, vector<int>, less<int>> pq;
         int i = 0;
         while (k-- > 0) {
             while (i < n && capitals[i].first <= w) {
-                pq.push(capitals[i++].second);
+                pq.emplace(capitals[i++].second);
             }
-            if (pq.empty()) break;
+            if (pq.empty()) {
+                break;
+            }
             w += pq.top();
             pq.pop();
         }
