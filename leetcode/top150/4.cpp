8bf6a85da690c941ce112711c833c6b3373d4fa7
Xinyao Qiu <xylonqiu@gmail.com>
2025-07-22 10:42:59 +0800

    Median of Two Sorted Arrays
    Change-Id: Id0fab87c37f99007e7bf7029fe48e373fad9bcec

--- leetcode/top150/4.cpp
+++ leetcode/top150/4.cpp
@@ -1,13 +1,17 @@
 #include <vector>
+#include <climits>
 using namespace std;
 
-// [1, 3, 5, 7(left)(i=3), 9(right)]
-// [2, 4, 6(j=2), 8, 10, 11]
+// [1, 3, 5, 7(left)(right), 9]
+// [2, 4, 6(j), 8, 10, 11]
 // 1, 2, 3, 4, 5, [6], 7, 8, 9, 10, 11
 // i + j = (m + n) / 2
-
-// [2(i=0)]
-// [1, 3(j=1)]
+// nums1(i): i++; possible->impossible
+// nums2(j): j--==i++; impossible->possible
+// only one (i, j) that nums1 possible & nums2 possible
+// so this *only one* must be the last one that nums1(i) possible
+// nums1: p p p p p i i i i 
+// nums2: i i i i i p p p p 
 
 class Solution {
 public:
@@ -18,23 +22,22 @@ public:
         int m = nums1.size(), n = nums2.size();
         int left = 0, right = m;
         while (left <= right) {
-            int i = (left + right) / 2;
-            int j = (m + n) / 2 - i;
-            if ((i == 0 || j == n || nums1[i - 1] <= nums2[j])) {
+            int i = (left + right) / 2, j = (m + n) / 2 - i;
+            if (i == 0 || nums1[i - 1] <= nums2[j]) {
                 left = i + 1;
             } else {
                 right = i - 1;
             }
         }
         int i = right, j = (m + n) / 2 - i;
-        int aMax1 = i > 0 ? nums1[i - 1] : INT_MIN;
-        int aMax2 = j > 0 ? nums2[j - 1] : INT_MIN;
-        int bMin1 = i < m ? nums1[i] : INT_MAX;
-        int bMin2 = j < n ? nums2[j] : INT_MAX;
+        int a1 = i > 0 ? nums1[i - 1] : INT_MIN;
+        int a2 = j > 0 ? nums2[j - 1] : INT_MIN;
+        int b1 = i < m ? nums1[i] : INT_MAX;
+        int b2 = j < n ? nums2[j] : INT_MAX;
         if ((m + n) % 2 == 1) {
-            return min(bMin1, bMin2);
+            return min(b1, b2);
         }
-        return (max(aMax1, aMax2) + min(bMin1, bMin2)) / 2.0;
+        return (max(a1, a2) + min(b1, b2)) / 2.0;
     }
 };
 
