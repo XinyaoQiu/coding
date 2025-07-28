ab64a80e850d3c9403ee676dcd3bbdc01f541078
Xinyao Qiu <xylonqiu@gmail.com>
2025-07-28 11:24:10 +0800

    69. Sqrt(x)
    Change-Id: I55de6ae283abf3b03a6ea32dcba06925b4d740fd

---
+++ leetcode/top150/69.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+class Solution {
+public:
+    int mySqrt(int x) {
+        int L = 0, R = x;
+        while (L <= R) {
+            int M = L + (R - L) / 2;
+            if ((long long)(M * M) <= x) {
+                L = M + 1;
+            } else {
+                R = M - 1;
+            }
+        }
+        return R;
+    }
+};
