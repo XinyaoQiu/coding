6f35f9aa3b3478254448ad7b5bea4f4a4be6fc9a
Xinyao Qiu <xylonqiu@gmail.com>
2025-08-04 21:50:31 +0800

    1
    Change-Id: I0ab27bb4f457f3572903afd5c6aa364efb860989

--- leetcode/top150/3.cpp
+++ leetcode/top150/3.cpp
@@ -5,6 +5,13 @@ using namespace std;
 class Solution {
   public:
     int lengthOfLongestSubstring(string s) {
-        
+        unordered_map<int, int> m;
+		int i = 0, ans = 0;
+		for (int j = 0; j < s.size(); ++j) {
+			i = max(i, m[s[j]]);
+			m[s[j]] = j + 1;
+			ans = max(ans, j - i + 1);
+		}
+		return ans;
     }
 };
