fd06254f60731ee8d84942db675d628d831a7eed
Xinyao Qiu <xylonqiu@gmail.com>
2025-10-01 17:38:29 -0500

    1

---
+++ leetcode/hot100_python/739.py
@@ -0,0 +1,13 @@
+from typing import *
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stk = []
+        ans = [0] * len(temperatures)
+        for i, t in enumerate(temperatures):
+            while stk and t > temperatures[stk[-1]]:
+                j = stk.pop()
+                ans[j] = i - j
+            stk.append(i)
+        return ans
+
