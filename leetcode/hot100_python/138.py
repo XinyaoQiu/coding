650aff862ef563e1251ebe593448ee40683bc3f1
Xinyao Qiu <xylonqiu@gmail.com>
2025-10-01 16:17:01 -0500

    1

---
+++ leetcode/hot100_python/138.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = val
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+        cur = head
+        while cur:
+            cur.next = Node(cur.val, cur.next)
+            cur = cur.next.next
+        cur = head
+        while cur.next:
+            if cur.random:
+                cur.next.random = cur.random.next
+            cur = cur.next.next
+        new_head = head.next
+        cur = head
+        while cur and cur.next:
+            tmp = cur.next
+            cur.next = cur.next.next
+            cur = tmp
+        return new_head
+
