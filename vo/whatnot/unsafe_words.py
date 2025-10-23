5dbac1c5f428e6b3e3273a3e18ddaceda2430afa
Xinyao Qiu <xylonqiu@gmail.com>
2025-10-22 21:12:05 -0500

    1

---
+++ vo/whatnot/unsafe_words.py
@@ -0,0 +1,42 @@
+UNSAFE_WORDS = [
+'Nintendo',
+'Microsoft',
+'SONY',
+'usb-C',
+]
+
+messages = [
+{
+    "username": 'tom',
+    "text": 'i am enjoying the nintendo switch',
+},
+{
+    "username": 'jerry',
+    "text": 'i prefer the wii',
+},
+{
+    "username": 'tom',
+    "text": 'my best friend is sonya',
+},
+{
+    "username": 'tom',
+    "text": 'do you have a usb charger',
+},
+{
+    "username": 'jerry',
+    "text": 'no but i do have a USB-C charger',
+},
+]
+# "dafNintendo-doo"
+# "Nintendo"
+# " Nintendo fdsa"
+
+unsafe_words = set()
+for word in UNSAFE_WORDS:
+    unsafe_words.add(word.lower())
+result = []
+for msg in messages:
+    for word in msg['text'].strip().split():
+        if word.lower() in unsafe_words:
+            result.append(msg['text'])
+print(result)
