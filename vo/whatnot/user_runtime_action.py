8806fcabcc8e13d0efbcdaa3a3519b9a57c4ffc2
Xinyao Qiu <xylonqiu@gmail.com>
2025-10-25 12:15:59 -0500

    1

--- vo/whatnot/user_runtime_action.py
+++ vo/whatnot/user_runtime_action.py
@@ -1,49 +1,63 @@
 from collections import defaultdict
 
 records = [
-    ['100', '1000', 'A'],
-    ['200', '1100', 'A'],
-    ['100', '1200', 'B'],
-    ['200', '1200', 'B'],
-    ['100', '1300', 'C'],
-    ['200', '1400', 'A'],
-    ['300', '1500', 'B'],
-    ['300', '1550', 'B'],
+    "100 1000 A",
+    "200 1100 A",
+    "100 1200 B",
+    "200 1200 B",
+    "100 1300 C",
+    "200 1400 A",
+    "300 1500 B",
+    "300 1550 B",
+    "100 1600 D",
 ]
 
-# {100: [A, B, C], 200: [A, B, A], 300: [B, B]}
-# {(A,): 2, (A, B): 2, (A, B, C): 1, (A, B, A): 1, (B,): 1, (B, B): 1}
+# {100: [A, B, C, D], 200: [A, B, A], 300: [B, B]}
+# 100: A 1 B 1 C 1 D 1
+# 200: A 1 B 1 A 1
+
+"""
+            0
+          /A \B
+         2    1
+        /B    \B
+       2       1
+      /C \A
+     1    1
+    /D    
+   1       
+"""
 
 """
 A 2
-    B 2
-        C 1
-        A 1
+    ->B 2
+        ->C 1
+        ->A 1
 B 1
-    B 1
+    ->B 1
 """
 
+class TrieNode:
+    def __init__(self):
+        self.count = 0
+        self.children = defaultdict(TrieNode)
 
-user_path = defaultdict(list)
-for user, time, action in records:
-    user_path[user].append((int(time), action))
-for user in user_path.keys():
-    user_path[user].sort()
-counts = defaultdict(int)
-for user, path in user_path.items():
-    path = [x[1] for x in path]
-    for i in range(len(path)):
-        counts[tuple(path[:i+1])] += 1
-print(dict(counts))
+user_paths = defaultdict(list)
+for record in records:
+    user, _, action = record.split()
+    user_paths[user].append(action)
 
-def dfs_print(prefix=(), indent=0):
-    nexts = [x for x in counts if x[:-1] == prefix]
-    for next in nexts:
-        print("    " * indent + f"{next[-1]} {counts[next]}")
-        dfs_print(next, indent + 1)
+root = TrieNode()
+for user, actions in user_paths.items():
+    node = root
+    for action in actions:
+        node = node.children[action]
+        node.count += 1
 
-dfs_print()
+def print_trie(node, indent=0):
+    for action, child in node.children.items():
+        prefix = "  " * indent + ("->" if indent else "") + f"{action} {child.count}"
+        print(prefix)
+        print_trie(child, indent + 1)
 
-
-    
-        
+print_trie(root)
