0c21843ccc0946b7d12b253eafa0a95a267525db
Xinyao Qiu <xylonqiu@gmail.com>
2025-11-13 11:18:00 -0600

    1

--- vo/ziphq/round1/house_robber_3.py
+++ vo/ziphq/round1/house_robber_3.py
@@ -7,14 +7,28 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def rob(self, root: Optional[TreeNode]) -> int:
-        def dfs(root):
-            if not root:
-                return 0, 0
-            l_selected, l_not_selected = dfs(root.left)
-            r_selected, r_not_selected = dfs(root.right)
-            selected = root.val + l_not_selected + r_not_selected
-            not_selected = max(l_selected, l_not_selected) + max(r_selected, r_not_selected)
-            return selected, not_selected
-        return max(dfs(root))
+
+def rob(root: Optional[TreeNode]) -> int:
+    def dfs(root):
+        if not root:
+            return 0, 0
+        l_selected, l_not_selected = dfs(root.left)
+        r_selected, r_not_selected = dfs(root.right)
+        selected = root.val + l_not_selected + r_not_selected
+        not_selected = max(l_selected, l_not_selected) + max(r_selected, r_not_selected)
+        return selected, not_selected
+    return max(dfs(root))
+
+def func2(roots):
+    def dfs(root):
+        if not root:
+            return 0, 0
+        l_selected, l_not_selected = dfs(root.left)
+        r_selected, r_not_selected = dfs(root.right)
+        selected = root.val + l_not_selected + r_not_selected
+        not_selected = max(l_selected, l_not_selected) + max(r_selected, r_not_selected)
+        return selected, not_selected
+    ret = 0
+    for root in roots:
+        ret += max(dfs(root))
+    return ret
